83332bd7e2bb3e130e5ae39c9ee9b15c290c05a8
Przemysław Barcicki <przemekbarcicki@gmail.com>
2023-06-24 19:42:45 +0200

    tidying up

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,21 @@
-# TODO: add builder
-FROM python:3.9.16-slim
+FROM python:3.9.16-slim AS base
 
-ENV PIPENV_VENV_IN_PROJECT=1 PYTHONDONTWRITEBYTECODE=1 PYTHONUNBUFFERED=1 PIP_DISABLE_PIP_VERSION_CHECK=1 PIP_NO_CACHE_DIR=1
-ENV PIPENV_CACHE_DIR=/tmp/cache
+ENV PIPENV_VENV_IN_PROJECT=1 PYTHONDONTWRITEBYTECODE=1 PYTHONUNBUFFERED=1 PIP_DISABLE_PIP_VERSION_CHECK=1
+
+FROM base AS builder
 # install pipenv
 RUN pip install --user pipenv
 
 # move Pipfiles into working directory
-ADD Pipfile.lock Pipfile /usr/src/
-WORKDIR /usr/src
+ADD Pipfile.lock Pipfile .
 
 # download all packages
-RUN /root/.local/bin/pipenv sync
+RUN /root/.local/bin/pipenv install --deploy --verbose
+
+FROM base as runtime
+WORKDIR /app/
+COPY --from=builder .venv .venv
 
 ADD src ./
+
 CMD [".venv/bin/python", "-m", "gunicorn", "-b", "0.0.0.0:5000", "-w", "1", "predictor:get_app()"]
--- src/predictor/preprocess/mapping.py
+++ src/predictor/preprocess/mapping.py
@@ -1,8 +1,6 @@
 # flake8: noqa: F405
 import typing as T
 
-from .bulk import *  # noqa: F405
-
 USABLE_PARAMETERS = {
     "batch-instance-seq",
     "batch-workflow",
@@ -20,39 +18,6 @@ USABLE_PARAMETERS = {
     "skip-mdl-landing",
     "skip-mdl-out",
     "source-type",
-}
-
-MAPPING: T.Dict[str, T.Callable[[str, T.Any], T.Dict[str, T.Union[int, str]]]] = {
-    "api-version": drop,
-    "approach": drop,
-    "as-of-date": drop,
-    "as-of-datetime": drop,
-    "batch-instance-seq": batch_instance_seq,
-    "batch-workflow": batch_workflow,
-    "bsinp-run-id": bsinp_run_id,
-    "business-date": drop,
-    "business-day": business_day,
-    "chf-usd-rate": drop,
-    "correlation-id": drop,
-    "failed-job-id": drop,
-    "failed-job-status": drop,
-    "failed-job-uid": drop,
-    "flow-type": drop,
-    "regulatory-authority": drop,
-    "hac-run-id": hac_run_id,
-    "ib-run-id": ib_run_id,
-    "pb-run-id": pb_run_id,
-    "process-flag": process_flag,
-    "processing-location": processing_location,
-    "rd-run-id": rd_run_id,
-    "regulatory-approaches": regulatory_approaches,
-    "rules-branch": rules_branch,
-    "scenario-workflow": drop,
-    "setenv": drop,
-    "skip-ler": skip_ler,
-    "skip-mdl-landing": skip_mdl_landing,
-    "skip-mdl-out": skip_mdl_out,
-    "source-type": source_type,
 }
 
 
